9d6d834d1622ae3b842a9668a35aa72c10db6d80
Unknown <barre.js@gmail.com>
2018-11-25 14:58:47 +0100

    initialisation bdd
    pour que la bdd ne s'initialise qu'une seule fois

--- class/App.php
+++ class/App.php
@@ -3,8 +3,12 @@
 // Le but est de créer une fonction static pour l'accès à la bdd
 class App{
 
+static $db = null;
 
   static function getDatabase(){
-    return new Database('root', '', 'photo');
+    if(!self::$db){
+      self::$db = new Database('root', '', 'photo');
+    }
+    return self::$db;
   }
 }
