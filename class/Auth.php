9a140e384b460fb9d835074e74f724eac8c8e642
Unknown <barre.js@gmail.com>
2018-11-27 18:40:08 +0100

    Réécriture de login.php
    Le code de reconnection où l'on teste  si le login et le mdp correspondent qui est dans le login a interrêt à être mis dans la partie authentification : la classe Auth
    Je crée donc une fonction login
    j'ai besoin de me connecter à la bdd donc j'ajoute le paramètre $db à ma fonction login...
    
    ma fonction login est un peu longue, je crée donc une fonction remember() Cela aura pour avantage lors d'un bug, de savoir si cela vient du cookie...

--- class/Auth.php
+++ class/Auth.php
@@ -52,4 +52,71 @@ class Auth{
             exit();
         }
     }
+
+    public function user(){
+        if(!$this->session->read('auth')){
+            return false;
+        }
+        return $this->session->read('auth');
+    }
+
+    public function connect($user){
+        $this->session->write('auth', $user);
+    }
+
+    public function connectFromCookie($db){
+
+        // if (isset($_COOKIE['remember']) && !isset($_SESSION['auth'])) {
+        if (isset($_COOKIE['remember']) && !$this->user()) {
+            // require_once 'db.php';
+            // if (!isset($pdo)) {
+            //     global $pdo;
+            // }
+    
+            $remember_token = $_COOKIE['remember'];
+            $parts = explode('==', $remember_token);
+            $user_id = $parts[0];
+            $user = $db->query('SELECT * FROM users WHERE id = ?', [$user_id])->fetch();
+            if ($user) {
+                $expected = $user_id . '==' . $user->remember_token . sha1($user_id . 'tetedeslip');
+                if ($expected == $remember_token) {
+                    // session_start();
+                    $this->connect($user);
+                    // $_SESSION['auth'] = $user;
+                    setcookie('remember', $remember_token, time() + 60 * 60 * 24 * 31);
+                } else {
+                    setcookie('remember', null, -1);
+                }
+                    } else {
+                setcookie('remember', null, -1);
+                    }
+        }
+    }
+
+    public function login($db, $username, $password, $remember = false) {
+        $user = $db -> query('SELECT * FROM users WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL', ['username' => $username])->fetch();
+        if (password_verify($_POST['password'], $user -> password)) {
+            // $_SESSION['auth'] = $user;
+            $this->connect($user);
+            // ce messages flash n'est plus ici
+            // $_SESSION['flash']['success'] = 'Vous êtes maintenant connecté';
+            if($remember){
+                $this->remember($db, $user->id);
+            }
+            header('Location: account.php');
+            exit();
+        } else {
+        $_SESSION['flash']['danger'] = 'Identifiant ou mot de passe incorrect';
+        }
+    }
+
+    public function remember($db, $user_id){
+        // if ($_POST['remember']) {
+        if($remember){ 
+            // $remember_token = str_random(250); car j'ai créé une fonction statique
+            $remember_token = Str::random();
+            $db -> query('UPDATE users SET remember_token = ? WHERE id = ?', [$remember_token, $user_id]);
+            setcookie('remember', $user_id.'=='.$remember_token.sha1($user_id .'tetedeslip'), time() + 60 * 60 * 24 * 15);
+        }
+    }
 }
--- login.php
+++ login.php
@@ -1,31 +1,30 @@
 <?php 
 require 'inc/bootstrap.php';
-require_once 'inc/functions.php';
-reconnect_from_cookie();
-if(isset($_SESSION['auth'])){
-	header('location: account.php');
-	exit();
+// require_once 'inc/functions.php';
+// reconnect_from_cookie();
+
+$auth = App::getAuth();
+// j'ai besoin de l'accès à la bdd dans la fonction connectFromCookie
+$db = App::getDatabase();
+$auth->connectFromCookie($db);
+// if(isset($_SESSION['auth'])){
+// 	header('location: account.php');
+// 	exit();
+// }
+
+// ($auth->user()) signifie que l'utilisateur est connecté
+
+if($auth->user()){
+	App::redirect('account.php');
 }
 if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
-	require_once 'inc/db.php';
-
-	$req = $pdo->prepare('SELECT * FROM users WHERE (username = :username OR email = :username) AND confirmed_at IS NOT NULL');
-	$req->execute(['username' => $_POST['username']]);
-	$user = $req->fetch();
-	
-	if(password_verify($_POST['password'], $user->password)){
-		$_SESSION['auth'] = $user;
+	$user = $auth->login($db, $_POST['username'], $_POST['password'], isset($_POST['remember']));
+	if($user){
 		$_SESSION['flash']['success'] = 'Vous êtes maintenant connecté';
-		if($_POST['remember']){
-			$remember_token = str_random(250);
-			$pdo->prepare('UPDATE users SET remember_token = ? WHERE id = ?')->execute([$remember_token, $user->id]);
-			setcookie('remember', $user->id . '==' . $remember_token . sha1($user->id .'tetedeslip'), time() + 60 * 60 * 24 * 15);
-		}
 		header('Location: account.php');
-		exit();
-	} else {
-	$_SESSION['flash']['danger'] = 'Identifiant ou mot de passe incorrect';
+        exit();
 	}
+
 }
 ?>
 
